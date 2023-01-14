d6fc287f792bcc57281c019d13c765e3dea95a01
Ubinquitous <ubinquitous1@gmail.com>
2023-01-15 04:17:56 +0900

    ADD : return

--- webtoon_app/lib/services/api.dart
+++ webtoon_app/lib/services/api.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:webtoon_app/models/webtoon_detail_model.dart';
+import 'package:webtoon_app/models/webtoon_episode_model.dart';
 import 'package:webtoon_app/models/webtoon_model.dart';
 
 class Api {
@@ -19,6 +21,30 @@ class Api {
         webtoonInstances.add(WebtoonModel.fromJson(webtoon));
       }
       return webtoonInstances;
+    }
+    throw Error();
+  }
+
+  static Future<WebtooneDetailModel> getToonById(String id) async {
+    final url = Uri.parse('$baseUrl/$id');
+    final res = await http.get(url);
+    if (res.statusCode == 200) {
+      final webtoon = jsonDecode(res.body);
+      return WebtooneDetailModel.fromJson(webtoon);
+    }
+    throw Error();
+  }
+
+  static Future<List<WebtoonEpisodeModel>> getLatestEpisodes(String id) async {
+    List<WebtoonEpisodeModel> episodesInstances = [];
+    final url = Uri.parse('$baseUrl/$id/episodes');
+    final res = await http.get(url);
+    if (res.statusCode == 200) {
+      final episodes = jsonDecode(res.body);
+      for (var episode in episodes) {
+        episodesInstances.add(WebtoonEpisodeModel.fromJson(episode));
+      }
+      return episodesInstances;
     }
     throw Error();
   }
